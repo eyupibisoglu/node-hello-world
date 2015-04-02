2efa90d469fa86dcf88bbdc52c7176aed8d08d85
eyupibisoglu <ibisoglueyup@gmail.com>
2015-04-03 00:00:47 +0300

    back to node

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,11 +1,13 @@
 
 
-var express = require('express'); 
-var app = express(); 
+var http = require('http');
 
-app.get('/', function(request, response)
-{ 
-	res.send('Hello World'); 
-}); 
+http.createServer(function (req, res) 
+{
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+  	res.end('Hello World\n');
 
-app.listen(3000);
+}).listen(1337);
+
+console.log('Server running at http://127.0.0.1:1337/');
+
